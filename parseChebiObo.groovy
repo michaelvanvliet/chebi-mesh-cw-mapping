d143777c513fd3b6cf83e2260262ce91728e7fdb
Michael van Vliet <info@michaelvanvliet.nl>
2013-12-02 12:57:49 +0100

    Added an option to use the OPS mappings from Chebi to Chemspider to look-up the CW uuid

--- parseChebiObo.groovy
+++ parseChebiObo.groovy
@@ -11,8 +11,43 @@ outputFile.delete() // make sure we create a new one
 new File('.cache/').mkdirs()
 
 def chebiId = ''
+def chemspiderId = ''
+def cwUuidFromChemspideId = ''
+def cheb2chem = [:]
 def lineLimit = 0
 def lineCount = 0
+
+def opsMapping = [:]
+// read in OPS 2 Chemspider
+new File('./private/LINKSET_EXACT_OPS_CHEMSPIDER_CHEBI20131111.ttl').eachLine { opschem ->
+	try {
+		def opschemParts = opschem.split(' ')
+		def opsId = opschemParts[0].replace('ops:', '')
+		def chemId = opschemParts[2].replace('<http://rdf.chemspider.com/', '').replace('>','')
+
+		opsMapping[opsId] = ['chemId': chemId]
+	} catch (e) {
+		// ignore incorrect formatted lines!
+	}
+}
+
+// read in Chebi 2 OPS mapping
+new File('./private/LINKSET_EXACT_CHEBI20131111.ttl').eachLine { opscheb ->
+	try {
+		def opschebParts = opscheb.split(' ')
+		def opsId = opschebParts[0].replace('ops:', '')
+		def chebId = "CHEBI:" + opschebParts[2].replace('<http://purl.obolibrary.org/obo/CHEBI_', '').replace('>','')
+
+		opsMapping[opsId] = opsMapping[opsId] + ['chebId': chebId]
+	} catch (e) {
+		// ignore incorrect formatted lines!
+	}
+}
+
+// build chebi to chemspider mapping
+opsMapping.each { opsId, m -> cheb2chem[m.chebId] = m.chemId}
+
+
 
 new File(locationOboFile).eachLine { oboLine ->
 
@@ -21,7 +56,9 @@ new File(locationOboFile).eachLine { oboLine ->
 	if (lineLimit == 0 || lineCount < lineLimit){
 
 		if (oboLine.size() > 5 && oboLine[0..3] == 'id: '){
-			chebiId	= oboLine[4..-1]
+			chebiId = oboLine[4..-1]
+			chemspiderId = cheb2chem[chebiId] ?: ''
+			cwUuidFromChemspideId = getCWuuidFromChemspideId(chemspiderId) ?: ''
 		}
 
 		if (oboLine.size() > 5 && oboLine[0..5] == 'name: '){
@@ -33,24 +70,80 @@ new File(locationOboFile).eachLine { oboLine ->
 
 			def meshPageHTML = getUrlContents('http://www.nlm.nih.gov/cgi/mesh/2013/MB_cgi?mode=&term=' + java.net.URLEncoder.encode(chebiName)) ?: ''
 
-			if (meshPageHTML.contains('Please select a term from list')){
-				def optionalMeshHits = getMultipleMeshResults(meshPageHTML)
-				if (optionalMeshHits.size() <= 10){ // to many hits is probably because it is to general!
-					optionalMeshHits.each { optionalMeshPage ->
-						meshPageHTML = getUrlContents(optionalMeshPage)
-						def meshDetails = getMeshDetailsFromHtml(meshPageHTML)
-						chebiOptions <<  "${chebiId}\t${chebiName}\t${meshDetails['meshId']}\t${meshDetails['meshName']}\t${meshDetails['uuidFromMeshWithIdAndName']}\t${meshDetails['uuidFromMeshWithName']}"
+			if (meshPageHTML.contains('No term found')){
+				chebiOptions <<  "${chebiId}\t${chebiName}\t${chemspiderId}\t.\t.\t${cwUuidFromChemspideId}\t.\t.\tEOL"
+			} else {
+				if (meshPageHTML.contains('Please select a term from list')){
+					def optionalMeshHits = getMultipleMeshResults(meshPageHTML)
+					if (optionalMeshHits.size() <= 10){ // to many hits is probably because it is to general!
+						optionalMeshHits.each { optionalMeshPage ->
+							meshPageHTML = getUrlContents(optionalMeshPage)
+							def meshDetails = getMeshDetailsFromHtml(meshPageHTML)
+							chebiOptions <<  "${chebiId}\t${chebiName}\t${chemspiderId}\t${meshDetails['meshId']}\t${meshDetails['meshName']}\t${cwUuidFromChemspideId}\t${meshDetails['uuidFromMeshWithIdAndName']}\t${meshDetails['uuidFromMeshWithName']}\tEOL"
+						}
+					} else {
+						chebiOptions <<  "${chebiId}\t${chebiName}\t${chemspiderId}\t.\t.\t${cwUuidFromChemspideId}\t.\t.\tEOL"
 					}
 				} else {
-					chebiOptions <<  "${chebiId}\t${chebiName}\t\t\t"
+					def meshDetails = getMeshDetailsFromHtml(meshPageHTML)
+					chebiOptions <<  "${chebiId}\t${chebiName}\t${chemspiderId}\t${meshDetails['meshId']}\t${meshDetails['meshName']}\t${cwUuidFromChemspideId}\t${meshDetails['uuidFromMeshWithIdAndName']}\t${meshDetails['uuidFromMeshWithName']}\tEOL"
 				}
-			} else {
-				def meshDetails = getMeshDetailsFromHtml(meshPageHTML)
-				chebiOptions <<  "${chebiId}\t${chebiName}\t${meshDetails['meshId']}\t${meshDetails['meshName']}\t${meshDetails['uuidFromMeshWithIdAndName']}\t${meshDetails['uuidFromMeshWithName']}"
 			}
 
 			chebiOptions.unique().each {
-				outputFile << "${lineCount}\t${it}\n"
+
+				// how do the UUIDs relate?
+				def lineScore = 9
+				def noOfFoundUUIDs = 0
+				def foundUUIDs = []
+				try {
+					if ("${it.split("\t")[-2]}".size() == 36) {
+						foundUUIDs << it.split("\t")[-2]
+						noOfFoundUUIDs++
+					// } else {
+					// 	foundUUIDs << 'CHEMUUID'
+					}
+
+					if ("${it.split("\t")[-3]}".size() == 36) {
+						foundUUIDs << it.split("\t")[-3]
+						noOfFoundUUIDs++
+					// } else {
+					// 	foundUUIDs << 'MESHIDNUUID'
+					}
+
+					if ("${it.split("\t")[-4]}".size() == 36) {
+						foundUUIDs << it.split("\t")[-4]
+						noOfFoundUUIDs++
+					// } else {
+					// 	foundUUIDs << 'MESHNUUID'
+					}
+
+					lineScore = foundUUIDs.unique().size()
+				} catch (e) {
+					// not all could be found, score is 0
+				}
+
+				// does Chemspider agree with (one of the) Mesh UUIDs
+				def chemspiderMeshHit = 0
+				if (
+					(lineScore <= 2) &&
+					(it.split("\t")[-4] != '') && (
+						(it.split("\t")[-4] == it.split("\t")[-3]) ||
+						(it.split("\t")[-4] == it.split("\t")[-2])
+					)
+				    ) {
+					chemspiderMeshHit = 1
+					//println "HIT: ${it.split("\t")[-2]} >> ${it.split("\t")[-3]} OR ${it.split("\t")[-4]}"
+				}
+
+				// calc overall score
+				def overallScore = 0
+				if (noOfFoundUUIDs >= 1) {
+					overallScore = ((noOfFoundUUIDs)/(lineScore+1))+chemspiderMeshHit
+					overallScore = Math.round(overallScore*10)
+				}
+
+				outputFile << "${lineCount}\t${overallScore}\t${noOfFoundUUIDs}\t${lineScore}\t${chemspiderMeshHit}\t${it}\n"
 			}
 		}
 	}
@@ -69,8 +162,8 @@ def getUrlContents(String url){
 		// return cache html
 		if (cacheFile.exists()){
 
-			println "from cache ${url} ... "
-			return cacheFile.text
+			//println "from cache ${url} ... "
+			//return cacheFile.text
 		}
 	} catch(e) {
 		// sometimes the name is too long for caching, then we ignore it and keep fetching it from the internet
@@ -81,7 +174,7 @@ def getUrlContents(String url){
 	def urlContents
 	try {
 		urlContents = new URL(url)?.text ?: ''
-		println "downloading ${url} ... "
+		//println "downloading ${url} ... "
 	} catch (Exception e) {
 		println "Was unable to download content: ${e}"
 	}
@@ -103,6 +196,7 @@ def getMeshDetailsFromHtml(String html){
 	meshDetails['meshName'] = getMeshName(html) ?: ''
 	meshDetails['uuidFromMeshWithIdAndName'] = getCWuuid(meshDetails['meshId'] + ' ' + meshDetails['meshName']) ?: ''
 	meshDetails['uuidFromMeshWithName'] = getCWuuid(meshDetails['meshName']) ?: ''
+
 	return meshDetails
 }
 
@@ -161,6 +255,29 @@ def getMeshName(String html){
 	}
 
 	return meshName
+}
+
+def getCWuuidFromChemspideId(String chempiderId){
+	def uuid = ''
+	def cwResponse
+
+	query = chempiderId.trim()
+
+	if (query.size() >= 3){
+		try {
+			def url = 'http://conceptwiki.nbiceng.net/web-ws/concept/search?q=' + java.net.URLEncoder.encode(query) + '&branch=4&limit=1'
+			cwResponse = getUrlContents(url)
+
+			def JsonSlurper = new JsonSlurper()
+			def cwConcept = JsonSlurper.parseText(cwResponse)
+			//println cwConcept
+			uuid = cwConcept.uuid[0] ?: ''
+		} catch (Exception e) {
+			// do nothing
+		}
+	}
+
+	return uuid
 }
 
 def getCWuuid(String query){
